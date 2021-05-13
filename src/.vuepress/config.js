709b2aba50161486b27374129ab98702d9ffd780
AFURO-1130 <softtennis6161130@icloud.com>
2021-05-13 23:20:30 +0900

    bagu

---
+++ src/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'About', link: '/about/' },
+        { text: 'Blog', link: 'https://www.nxworld.net/' },
+        {
+            text: 'More',
+            items: [
+              { text: 'Twitter', link: 'https://twitter.com/' },
+              { text: 'GitHub', link: 'https://github.com/' },
+              { text: 'Dribbble', link: 'https://dribbble.com/' }
+            ]
+          }
+          
+      ],
+      sidebar: [
+      '/',
+      '/about/'
+    ],
+    displayAllHeaders: true
+    }
+  }
