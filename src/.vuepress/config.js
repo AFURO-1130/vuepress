d3f36bc8cb69f93b3df82c1a68ceb4af02de599d
AFURO-1130 <softtennis6161130@icloud.com>
2021-05-13 23:40:13 +0900

    ほげ

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,23 +1,20 @@
-module.exports = {
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'About', link: '/about/' },
-        { text: 'Blog', link: 'https://www.nxworld.net/' },
-        {
-            text: 'More',
-            items: [
-              { text: 'Twitter', link: 'https://twitter.com/' },
-              { text: 'GitHub', link: 'https://github.com/' },
-              { text: 'Dribbble', link: 'https://dribbble.com/' }
-            ]
-          }
-          
-      ],
-      sidebar: [
-      '/',
-      '/about/'
-    ],
-    displayAllHeaders: true
+export const themeConfig = {
+  nav: [
+    { text: 'Home', link: '/' },
+    { text: 'About', link: '/about/' },
+    { text: 'Blog', link: 'https://www.nxworld.net/' },
+    {
+      text: 'More',
+      items: [
+        { text: 'Twitter', link: 'https://twitter.com/' },
+        { text: 'GitHub', link: 'https://github.com/' },
+        { text: 'Dribbble', link: 'https://dribbble.com/' }
+      ]
     }
-  }
+  ],
+  sidebar: [
+    '/',
+    '/about/'
+  ],
+  displayAllHeaders: true
+};
